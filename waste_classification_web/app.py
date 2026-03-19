161f39fe10180d7c5e74bffc8c75620869cadf23
hoanglamhieu <hoanglamhieu2509@gmail.com>
2026-03-19 22:07:12 +0700

    Fix port for Render deployment

--- waste_classification_web/app.py
+++ waste_classification_web/app.py
@@ -91,6 +91,10 @@ def index():
         softmax=softmax
     )
 
+
+
 if __name__ == '__main__':
-    
-    app.run(debug=True)
+    # Lấy cổng từ môi trường của Render, nếu không có thì mặc định là 5000
+    port = int(os.environ.get('PORT', 5000))
+    # Chạy ứng dụng với host 0.0.0.0
+    app.run(host='0.0.0.0', port=port)
